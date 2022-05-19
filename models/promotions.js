3d91995648d5d1e6eafcaf8dca2bd985962165fc
samir <samirsamawade@gmail.com>
2022-05-20 00:00:51 +0300

    Express with MongoDB and Mongoose Part 3

---
+++ models/promotions.js
@@ -0,0 +1,36 @@
+const { model } = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+},
+{
+    timestamp: true
+});
+
+var Leaders = mongoose.model('leader', leaderSchema);
+
+module.exports = Leaders;
