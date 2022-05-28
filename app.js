0a79dcb16211e434c7c83056b4d57757193747a1
samir <samirsamawade@gmail.com>
2022-05-28 17:18:11 +0300

    Cookies

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
-connect.then((db)=> {
+connect.then((db) => {
   console.log('Connected correctely to server');
-}, (err)=> { console.log(err) });
+}, (err) => { console.log(err) });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,33 +29,45 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth(req, res, next){
-  console.log(req.headers);
+function auth(req, res, next) {
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error('You are not authorized!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+    }
 
-  if (!authHeader){
-    var err = new Error('You are not authorized!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
 
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-
-  if(user === 'admin' && pass === 'password'){
-    next(); //Authoriced
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', {signed: true});
+      next(); //Authoriced
+    }
+    else {
+      var err = new Error('You are not authorized!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      res.statusCode = 401;
+      next(err);
+    }
   }
   else {
-    var err = new Error('You are not authorized!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    res.statusCode = 401;
-    next(err);
+    if(req.signedCookies.user === 'admin'){
+      next();
+    }
+    else{
+      var err = new Error('You are not authorized!');
+      err.status = 401;
+      next(err);
+    }
   }
 
 }
@@ -66,17 +78,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
