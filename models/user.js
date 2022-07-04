b15e4e5467e1dc482a33c7fb33f4c9e473f8c6bc
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-07-04 10:22:00 +0300

    added required in User First Name & Last Name

--- models/user.js
+++ models/user.js
@@ -1,21 +1,21 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var passportLocalMongoose = require('passport-local-mongoose');
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var User = new Schema({
-    firstname: {
-        type: String,
-          default: ''
-      },
-      lastname: {
-        type: String,
-          default: ''
-      },
-    admin: {
-        type: String,
-        default: false
-    }
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  admin: {
+    type: String,
+    default: false,
+  },
 });
 
 User.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model("User", User);
